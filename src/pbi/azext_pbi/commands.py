79cfa6a606406280474f3c0e9b9e3b84ba9ed8ab
Andrei Zhozhin <andrei.zhozhin@gmail.com>
2023-11-09 20:00:45 +0000

    add index.json

--- src/pbi/azext_pbi/commands.py
+++
@@ -1,7 +0,0 @@
-def load_command_table(self, args):
-    with self.command_group('pbi') as g:
-        g.custom_command('version', 'pbi_version')
-
-    with self.command_group('pbi workspace') as g:
-        g.custom_command('list', 'pbi_workspace_list')
-        g.custom_command('show', 'pbi_workspace_show')
